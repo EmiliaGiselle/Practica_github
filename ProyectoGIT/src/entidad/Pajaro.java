662a7fae10dd3804b8c66b43ed5320fcd300780b
Natalia Alcaraz <nv.alcaraz@gmaail.com>
2021-10-15 10:16:02 -0300

    agregue atributos a clase Pajaro

--- ProyectoGIT/src/entidad/Pajaro.java
+++ ProyectoGIT/src/entidad/Pajaro.java
@@ -1,14 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package entidad;
 
-/**
- *
- * @author Nati
- */
 public class Pajaro {
+    public String nombre;
+    public String habilidad;
+    public String tamaño;
+    public String color;
     
 }
