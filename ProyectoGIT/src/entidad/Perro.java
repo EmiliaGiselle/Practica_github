9270e8f3da1eb6eb83b29a86a9f44fb4f2e01ac9
CarlaIovchev <velichka272@gmail.com>
2021-10-18 12:17:18 -0300

    se suma perrito

--- ProyectoGIT/src/entidad/Perro.java
+++ ProyectoGIT/src/entidad/Perro.java
@@ -12,6 +12,42 @@ package entidad;
 public class Perro {
     private String color;
     private String nombre;
+    private String raza;
+    
+    
+
+    public Perro() {
+    }
+
+    public Perro(String color, String nombre, String raza) {
+        this.color = color;
+        this.nombre = nombre;
+        this.raza = raza;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getRaza() {
+        return raza;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setRaza(String raza) {
+        this.raza = raza;
+    }
     
     
 }
